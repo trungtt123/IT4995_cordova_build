85c407351fcf067fbc765cdd70315a03a9b00582
Trung Tran <bruceleemax111@gmail.com>
2023-04-17 07:18:14 +0700

    build android

---
+++ www/precache-manifest.b0f582af580cf0e984acfccf54ed17ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43c065a3b8f2bda0cd9ba4bd4f55e61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8f2ba8d8bf14632ad06",
+    "url": "/static/css/main.346d697f.chunk.css"
+  },
+  {
+    "revision": "b054f47b1111754e1280",
+    "url": "/static/js/2.9f4c2aa8.chunk.js"
+  },
+  {
+    "revision": "b8f2ba8d8bf14632ad06",
+    "url": "/static/js/main.9f0d28e3.chunk.js"
+  },
+  {
+    "revision": "e9d2d5a152643c563c4e",
+    "url": "/static/js/runtime-main.d74937b1.js"
+  },
+  {
+    "revision": "5442cef93dbada002e49a970fece3ccd",
+    "url": "/static/media/soict.5442cef9.png"
+  }
+]);
